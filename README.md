c847b4d0b07720d0b2c17bbd19d5763d92d802d5
azim <mdazimmia53@gmail.com>
2024-05-02 16:44:21 +0600

    add file

--- README.md
+++
@@ -1,1 +0,0 @@
-# useCallback-useMemo-problem-solved
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+  <p>useCallback,useMemo</p>
+    </>
+  )
+}
+
+export default App
